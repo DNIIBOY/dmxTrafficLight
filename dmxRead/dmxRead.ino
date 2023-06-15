191caf01458f46c01686a7fc6f6787dde1d45ec9
DNIIBOY <daniel@nettelfield.com>
2023-06-15 17:23:04 +0200

    first working dmx read

--- dmxRead/dmxRead.ino
+++ dmxRead/dmxRead.ino
@@ -1,15 +1,26 @@
 #include <DMXSerial.h>  // https://github.com/mathertel/DMXSerial
 
+#define RED 22
+#define YELLOW 23
+#define GREEN 24
+
+int startAddress = 1;
+
 void setup() {
   DMXSerial.init(DMXReceiver);
+  pinMode(RED, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  int dmxVal = DMXSerial.read(1);
-  if (dmxVal > 128){
-    digitalWrite(4, HIGH);
-  }
-  else{
-    digitalWrite(4, LOW);
-  }
+  unsigned long lastPacketTime = DMXSerial.noDataSince();
+  int redVal = DMXSerial.read(startAddress);
+  int yellowVal = DMXSerial.read(startAddress + 1);
+  int greenVal = DMXSerial.read(startAddress + 2);
+  
+  digitalWrite(RED, redVal > 128);
+  digitalWrite(YELLOW, yellowVal > 128);
+  digitalWrite(GREEN, greenVal > 128);
 }
